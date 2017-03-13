6dfe06f55cf12b5df5d416433c4ee48f22657d1b
Jane Wie <janewie@gmail.com>
2017-03-13 16:10:11 -0700

    create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
+} */
+
+var xyz = document.getElementById('game-board');
+for(var i=0; i < 4; i++) {
+   var createCards = document.createElement('div');
+   createCards.className = 'card';
+   xyz.appendChild(createCards);
 }
+
+createCards;
